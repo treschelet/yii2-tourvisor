166d2ffa271449f0a4d8156ad262adfac91c75ae
Evgeniy Krokun <treschelet@fry.(none)>
2014-08-06 09:19:27 +0400

    fix rules list Api

--- Module.php
+++ Module.php
@@ -27,7 +27,7 @@ class Module extends \yii\base\Module implements BootstrapInterface
     {
         $app->getUrlManager()->addRules([
             $this->id => $this->id . '/api/index',
-            $this->id . '/list' => $this->id . '/api/list',
+            $this->id . '/<action:(list|search|request)>' => $this->id . '/api/<action>',
             $this->id . '/list/<type:(departure|country|region|meal|stars|hotel|operator|flydate)>' => $this->id . '/api/list',
         ], false);
     }
